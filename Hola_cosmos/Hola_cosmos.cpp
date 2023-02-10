2560229bcf3457fecea325cb66dc6b4f613f0f0f
saramendez701 <saramendeztojin701@gmail.com>
2023-02-09 18:50:19 -0600

    Se agrega el Proyecto

---
+++ Hola_cosmos/Hola_cosmos.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <Windows.h>
+#include <clocale>
+
+int main()
+{
+    std::cout << "Sara Gabriela Méndez Tojín\n";
+    std::cout << "202307027\n";
+    std::cout << "Escogí la carrera porque siempre me interesó poder crear cosas que ayuden y que sean complejas, previamente había estudiado\n";
+    std::cout << "mercadotecnia donde pude explotar la creatividad, ahora quisiera poder conocer cosas técnicas y dar vida a mis ideas\n";
+    return 0;
+}
